a95dfb4f7b5016db211c8546bb15ef57e6c05f17
Sancheopance <sancheopance@gmail.com>
2020-11-16 20:08:44 +0800

    puls6

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,7 @@ $(document).ready(function(){
                 }
         ]
 
+
     });
 
 
@@ -99,6 +100,49 @@ $(document).ready(function(){
         };
         toggleSlide('.catalog-item__link');
         toggleSlide('.catalog-item__back');
+
+        // Modal-ные окна
+        $('[data-modal=consultation]').on('click', function(){
+            $('.overlay, #consultation').fadeIn('slow');
+        });
+        $('.modal__close').on('click', function(){
+            $('.overlay, #consultation, #thanks , #order').fadeOut('slow');
+        });
+
+ 
+        $('.button_mini').each(function(i){
+            $(this).on('click', function(){
+                $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+                    $('.overlay, #order').fadeIn('slow');
+            });
+        });
+
+
+  
+        function valideForms(form){
+            $(form).validate({
+                rules: {
+                    name: "required",
+                    phone: "required",
+                    email: {
+                      required: true,
+                      email: true
+                    }
+                },
+                messages: {
+                    name: "Пожалуйста, введите имя",
+                    phone: "Пожалуйста, введите ваш номер телефона" ,
+                    email: {
+                      required: "Введите свою почту",
+                      email: " Неправильный формат почты"
+                    }
+                }   
+            });
+        }
+        valideForms('#consultation-form');
+        valideForms('#consultation form');
+        valideForms('#order form');
+});
      
-    });
+
     
